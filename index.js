f7b08d9a3d8c6ca36a63d1df32eae69b77c9e98e
an2arch <kartovcev01@mail.ru>
2022-07-03 21:10:41 +0300

    Basic shape

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const canvas = document.getElementById("app");
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = document.documentElement.clientWidth;
-canvas.height = document.documentElement.clientHeight;
-
-ctx.fillRect(canvas.width * 0.1, canvas.height * 0.1, canvas.width * 0.8, canvas.height * 0.8);
+// ctx.fillRect(canvas.width * 0.1, canvas.height * 0.1, canvas.width * 0.8, canvas.height * 0.8);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
