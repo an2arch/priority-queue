2708479458ba38f66d32acacc8693ecbe738e77b
DmitryKHTV <dimah877@gmail.com>
2022-07-04 21:14:38 +0300

    indexes fix

--- DecQueue.js
+++ DecQueue.js
@@ -1,12 +1,11 @@
 class DecQueue {
 
     buffer = []
+
     constructor(list) {
-        if (list)
-        {
+        if (list) {
             this.buffer = list;
-            for (let i = this.buffer.length / 2 - 1; i>=0; --i)
-            {
+            for (let i = this.buffer.length % 2 - 1; i >= 0; --i) {
                 this.SiftDown(i);
             }
         }
@@ -29,11 +28,9 @@ class DecQueue {
     };
 
     SiftUp(idx) {
-        while(idx > 0)
-        {
-            let parent = (idx - 1)%2;
-            if (this.buffer[parent] >= this.buffer[idx])
-            {
+        while (idx > 0) {
+            let parent = (idx - 1) % 2;
+            if (this.buffer[parent] >= this.buffer[idx]) {
                 return;
             }
             [this.buffer[parent], this.buffer[idx]] = [this.buffer[idx], this.buffer[parent]];
@@ -41,27 +38,23 @@ class DecQueue {
         }
     };
 
-    Enqueue(item)
-    {
+    Enqueue(item) {
         this.buffer.push(item);
         this.SiftUp(this.buffer.length - 1);
     }
 
-    DeQueue()
-    {
+    DeQueue() {
         console.assert(this.buffer.length > 0);
         let result = this.buffer[0];
         this.buffer[0] = this.buffer.pop();
 
-        if (this.buffer.values())
-        {
+        if (this.buffer.values()) {
             this.SiftDown(0);
         }
         return result;
     }
 
-    Peek()
-    {
+    Peek() {
         console.assert(this.buffer.length > 0);
 
         return this.buffer[0];
