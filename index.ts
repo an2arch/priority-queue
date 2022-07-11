79f7e5077ec704886406f34d5697fd9d8cf544f3
DmitriyKHTV <dimah877@gmail.com>
2022-07-11 19:18:29 +0300

    Class Addition

--- index.ts
+++ index.ts
@@ -3,55 +3,6 @@
 type HexColor = `#${string}`;
 type Point = { x: number; y: number };
 
-// TODO: state variables
-const ITEM_SPACING_X: number = 40;
-const ITEM_BOX_PADDING: number = 10;
-const RESET_WIDTH = 110;
-const RESET_HEIGTH = 30;
-const RESET_POS: Point = { x: 0, y: 0 };
-const queue: DecQueue = new DecQueue();
-let currMatrix: DOMMatrix = new DOMMatrix();
-let scale: number = 1;
-let trace: number[][] = [];
-
-const canvas_container: HTMLDivElement = document.getElementById("canvas-container") as HTMLDivElement;
-const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
-const story: HTMLDivElement = document.getElementById("overflow") as HTMLDivElement;
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-const textBox = document.getElementById("text-box") as HTMLInputElement;
-const addButton = document.getElementById("add-button") as HTMLDivElement;
-const popButton = document.getElementById("pop-button") as HTMLDivElement;
-
-function initContext(ctx: CanvasRenderingContext2D): void {
-    ctx.font = "bold 18px OpenSans";
-    ctx.textBaseline = "middle";
-    ctx.resetTransform();
-}
-
-function updateSizes(canvas: HTMLCanvasElement) {
-    let container = canvas.parentElement || document.documentElement;
-    canvas.style.display = "none";
-
-    let rect = container.getBoundingClientRect();
-    let container_height = rect.height;
-    let container_width = rect.width;
-
-    let ratio = 4 / 3;
-
-    let canvas_height = container_height;
-    let canvas_width = canvas_height * ratio;
-
-    if (canvas_width > container_width) {
-        canvas_width = container_width;
-        canvas_height = canvas_width / ratio;
-    }
-    canvas.width = canvas_width;
-    canvas.height = canvas_height;
-    canvas.style.display = "block";
-
-    RESET_POS.x = canvas.width - RESET_WIDTH;
-}
-
 function getPoint(canvas: HTMLCanvasElement, point: Point): Point {
     let rect: DOMRect = canvas.getBoundingClientRect(); // abs. size of element
     let scaleX: number = canvas.width / rect.width; // relationship bitmap vs. element for x
@@ -82,201 +33,295 @@ function drawLine(ctx: CanvasRenderingContext2D, pointFrom: Point, pointTo: Poin
     ctx.stroke();
 }
 
-function drawItem(
-    ctx: CanvasRenderingContext2D,
-    point: Point,
-    item: number,
-    rectColor: HexColor,
-    textColor: HexColor
-): void {
-    let textWidth = ctx.measureText(String(item)).width;
-    let textHeight = 18;
-
-    let cx: number = point.x - textWidth / 2;
-    let cy: number = point.y - textHeight / 2;
-
-    ctx.fillStyle = rectColor;
-    ctx.fillRect(
-        cx - ITEM_BOX_PADDING,
-        cy - ITEM_BOX_PADDING,
-        textWidth + 2 * ITEM_BOX_PADDING,
-        textHeight + 2 * ITEM_BOX_PADDING
-    );
-
-    ctx.fillStyle = textColor;
-    ctx.fillText(String(item), cx, cy + textHeight / 2);
-}
-
-function drawItems(ctx: CanvasRenderingContext2D, items: number[]) {
-    // TODO: this may be state variables
-    let levelCount = Math.floor(Math.log2(items.length));
-    let positions: Point[] = [];
-
-    for (let i = 0; i < items.length; ++i) {
-        let levelCurrent = Math.floor(Math.log2(i + 1));
-        let countChildren = Math.pow(2, levelCount - levelCurrent);
-        let y = canvas.height / 2 - (levelCount * 50) / 2 + levelCurrent * 50;
-        let x =
-            i !== 0
-                ? positions[Math.ceil(i / 2) - 1].x - Math.pow(-1, (i + 1) % 2) * ITEM_SPACING_X * countChildren
-                : ctx.canvas.width / 2;
-        positions[i] = { x, y };
-    }
-
-    for (let i = 0; i < items.length; ++i) {
-        let parrent = Math.floor((i - 1) / 2);
-        if (parrent >= 0) {
-            drawLine(ctx, positions[i], positions[parrent], "#0f4844");
-        }
-    }
-    for (let i = 0; i < items.length; ++i) {
-        drawItem(ctx, positions[i], items[i], "#e494ae", "#0f4844");
-    }
-}
-
-function drawResetButton(ctx: CanvasRenderingContext2D) {
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(RESET_POS.x, RESET_POS.y, RESET_WIDTH, RESET_HEIGTH);
-    ctx.strokeRect(RESET_POS.x, RESET_POS.y, RESET_WIDTH, RESET_HEIGTH);
-
-    let text: string = "Reset view";
-    let textWidth: number = ctx.measureText(text).width;
-    ctx.fillStyle = "#0f4844";
-    ctx.textBaseline = "middle";
-    ctx.fillText(text, RESET_POS.x + (RESET_WIDTH - textWidth) / 2, RESET_HEIGTH / 2);
-}
-
 function getCurrentTimeStr(): string {
     return new Date().toLocaleTimeString();
 }
 
-function addToHistory(message: string): void {
-    story.insertAdjacentHTML("afterbegin", `<div class="info-container">
-        <div class="messages">${message}</div>
-        <div class="time">${getCurrentTimeStr()}</div>
-    </div>`);
-}
-function handleAddItem(textBox: HTMLInputElement): void {
-    if (!textBox.value) {
-        textBox.style.borderColor = "#FF3030";
-        return;
-    }
-    textBox.style.borderColor = "#000";
+class QueueWidget {
+    private readonly ITEM_SPACING_X: number = 40;
+    private readonly ITEM_BOX_PADDING: number = 10;
+    private readonly RESET_WIDTH: number = 110;
+    private readonly RESET_HEIGTH: number = 30;
+    private currMatrix: DOMMatrix = new DOMMatrix();
+    private scale: number = 1;
+    private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
 
-    const item = parseFloat(textBox.value);
-    if (!isNaN(item)) {
-        textBox.value = "";
-        trace = [];
-        queue.Enqueue(item, (s) => {
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+        this.initContext();
+        this.updateSizes();
+
+        this.canvas.onwheel = (e: WheelEvent) => {
+            this.handleScale(1.1, e.deltaY < 0, getPoint(canvas, { x: e.clientX, y: e.clientY }));
+        };
+
+        this.canvas.onmousemove = (e: MouseEvent) => {
+            let resetPos: Point = { x: this.canvas.width - this.RESET_WIDTH, y: 0 };
+            if (
+                rectContainPoint(
+                    resetPos,
+                    this.RESET_WIDTH,
+                    this.RESET_HEIGTH,
+                    getPoint(this.canvas, { x: e.clientX, y: e.clientY })
+                )
+            ) {
+                this.canvas.style.cursor = "pointer";
+            } else {
+                this.canvas.style.cursor = "move";
+            }
+        };
+
+        let savedOnMouseMove = this.canvas.onmousemove;
+
+        this.canvas.onmousedown = (e: MouseEvent) => {
+            let resetPos: Point = { x: this.canvas.width - this.RESET_WIDTH, y: 0 };
+            if (
+                rectContainPoint(
+                    resetPos,
+                    this.RESET_WIDTH,
+                    this.RESET_HEIGTH,
+                    getPoint(this.canvas, { x: e.clientX, y: e.clientY })
+                )
+            ) {
+                this.scale = 1;
+                this.currMatrix = new DOMMatrix();
+            }
+
+            this.canvas.onmousemove = (e: MouseEvent) => {
+                let offsetX: number = e.movementX;
+                let offsetY: number = e.movementY;
+                this.currMatrix.translateSelf(offsetX / this.scale, offsetY / this.scale);
+            };
+
+            this.canvas.onmouseup = () => {
+                this.canvas.onmousemove = savedOnMouseMove;
+                this.canvas.onmouseup = null;
+            };
+        };
+
+        this.canvas.onmouseup = () => {
+            this.canvas.onmousemove = savedOnMouseMove;
+            this.canvas.onmouseup = null;
+        };
+
+        window.addEventListener(
+            "load",
+            () => {
+                this.updateSizes();
+                this.initContext();
+            },
+            false
+        );
+
+        window.addEventListener(
+            "resize",
+            () => {
+                this.updateSizes();
+                this.initContext();
+            },
+            false
+        );
+    }
+
+    render(queue: DecQueue): void {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawResetButton();
+        this.ctx.save();
+        this.ctx.setTransform(this.currMatrix);
+        this.drawItems(queue.GetBuffer());
+        this.ctx.restore();
+    }
+
+    private initContext(): void {
+        this.ctx.font = "bold 18px OpenSans";
+        this.ctx.textBaseline = "middle";
+        this.ctx.resetTransform();
+    }
+
+    private updateSizes() {
+        let container: HTMLElement = this.canvas.parentElement || document.documentElement;
+        let savedDisplay = this.canvas.style.display;
+        this.canvas.style.display = "none";
+
+        let rect = container.getBoundingClientRect();
+        let container_height = rect.height;
+        let container_width = rect.width;
+
+        let ratio = 4 / 3;
+
+        let canvas_height = container_height;
+        let canvas_width = canvas_height * ratio;
+
+        if (canvas_width > container_width) {
+            canvas_width = container_width;
+            canvas_height = canvas_width / ratio;
+        }
+        this.canvas.width = canvas_width;
+        this.canvas.height = canvas_height;
+        this.canvas.style.display = savedDisplay;
+    }
+
+    private drawItem(point: Point, item: number, rectColor: HexColor, textColor: HexColor): void {
+        let textWidth = this.ctx.measureText(String(item)).width;
+        let textHeight = 18;
+
+        let cx: number = point.x - textWidth / 2;
+        let cy: number = point.y - textHeight / 2;
+
+        this.ctx.fillStyle = rectColor;
+        this.ctx.fillRect(
+            cx - this.ITEM_BOX_PADDING,
+            cy - this.ITEM_BOX_PADDING,
+            textWidth + 2 * this.ITEM_BOX_PADDING,
+            textHeight + 2 * this.ITEM_BOX_PADDING
+        );
+
+        this.ctx.fillStyle = textColor;
+        this.ctx.fillText(String(item), cx, cy + textHeight / 2);
+    }
+
+    private drawItems(items: number[]) {
+        // TODO: this may be state variables
+        let levelCount = Math.floor(Math.log2(items.length));
+        let positions: Point[] = [];
+
+        for (let i = 0; i < items.length; ++i) {
+            let levelCurrent = Math.floor(Math.log2(i + 1));
+            let countChildren = Math.pow(2, levelCount - levelCurrent);
+            let y = this.canvas.height / 2 - (levelCount * 50) / 2 + levelCurrent * 50;
+            let x =
+                i !== 0
+                    ? positions[Math.ceil(i / 2) - 1].x -
+                    Math.pow(-1, (i + 1) % 2) * this.ITEM_SPACING_X * countChildren
+                    : this.ctx.canvas.width / 2;
+            positions[i] = { x, y };
+        }
+
+        for (let i = 0; i < items.length; ++i) {
+            let parrent = Math.floor((i - 1) / 2);
+            if (parrent >= 0) {
+                drawLine(this.ctx, positions[i], positions[parrent], "#0f4844");
+            }
+        }
+        for (let i = 0; i < items.length; ++i) {
+            this.drawItem(positions[i], items[i], "#e494ae", "#0f4844");
+        }
+    }
+
+    private drawResetButton() {
+        this.ctx.fillStyle = "#FFF";
+        let resetPos: Point = { x: this.ctx.canvas.width - this.RESET_WIDTH, y: 0 };
+        this.ctx.fillRect(resetPos.x, resetPos.y, this.RESET_WIDTH, this.RESET_HEIGTH);
+        this.ctx.strokeRect(resetPos.x, resetPos.y, this.RESET_WIDTH, this.RESET_HEIGTH);
+
+        let text: string = "Reset view";
+        let textWidth: number = this.ctx.measureText(text).width;
+        this.ctx.fillStyle = "#0f4844";
+        this.ctx.textBaseline = "middle";
+        this.ctx.fillText(text, resetPos.x + (this.RESET_WIDTH - textWidth) / 2, this.RESET_HEIGTH / 2);
+    }
+
+    private handleScale(scaleMult: number, zoom: boolean, origin: Point) {
+        scaleMult = zoom ? scaleMult : 1 / scaleMult;
+
+        let imatrix: DOMMatrix = this.currMatrix.inverse();
+        origin.x = origin.x * imatrix.a + origin.y * imatrix.c + imatrix.e;
+        origin.y = origin.x * imatrix.b + origin.y * imatrix.d + imatrix.f;
+
+        if (this.scale * scaleMult >= 0.125 && this.scale * scaleMult <= 4) {
+            this.currMatrix.scaleSelf(scaleMult, scaleMult, 1, origin.x, origin.y);
+            this.scale *= scaleMult;
+        }
+    }
+}
+
+class HistoryContainer{
+    private story: HTMLDivElement;
+    constructor(story: HTMLDivElement) {
+        this.story = story;
+    }
+
+    addToHistory(message: string): void {
+        this.story.insertAdjacentHTML("afterbegin", `<div class="info-container">
+            <div class="messages">${message}</div>
+            <div class="time">${this.getCurrentTimeStr()}</div>
+        </div>`)
+    }
+
+    private getCurrentTimeStr(): string {
+        return new Date().toLocaleTimeString();
+    }
+}
+
+class FunctionsContainer {
+    private addButton: HTMLDivElement;
+    private popButton: HTMLDivElement;
+    private textBox: HTMLInputElement;
+    private history: HistoryContainer;
+
+    constructor(addButton: HTMLDivElement, popButton: HTMLDivElement, textBox: HTMLInputElement, story: HistoryContainer, queue: DecQueue) {
+        this.addButton = addButton;
+        this.popButton = popButton;
+        this.textBox = textBox;
+        this.history = story;
+
+        this.addButton.onclick = () => {
+            this.handleAddItem(queue);
+        };
+        this.popButton.onclick = () =>{this.handlePopItem(queue)};
+        this.textBox.onkeydown = (e) => {
+            if (e.key === "Enter") this.handleAddItem(queue);
+        };
+    }
+
+    handleAddItem(decQueue: DecQueue): void {
+        if (!this.textBox.value) {
+            this.textBox.style.borderColor = "#FF3030";
+            return;
+        }
+        this.textBox.style.borderColor = "#000";
+
+        const item = parseFloat(this.textBox.value);
+        if (!isNaN(item)) {
+            this.textBox.value = "";
+            let trace = [];
+            decQueue.Enqueue(item, (s) => {
+                trace.push([...s]);
+            });
+            this.history.addToHistory(`add ${item}`);
+        }
+    }
+
+    handlePopItem(decQueue: DecQueue): void {
+        let trace = [];
+        let result = decQueue.Dequeue((s) => {
             trace.push([...s]);
         });
-        addToHistory(`add ${item}`);
+
+        if (result === null) {
+            alert("There are no items in a queue");
+            return;
+        }
+        this.history.addToHistory(`pop ${result}`);
     }
 }
 
-function handlePopItem(): void {
-    trace = [];
-    let result = queue.Dequeue((s) => {
-        trace.push([...s]);
-    });
+const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
+const story: HTMLDivElement = document.getElementById("overflow") as HTMLDivElement;
+const textBox = document.getElementById("text-box") as HTMLInputElement;
+const addButton = document.getElementById("add-button") as HTMLDivElement;
+const popButton = document.getElementById("pop-button") as HTMLDivElement;
 
-    if (result === null) {
-        alert("There are no items in a queue");
-        return;
-    }
-    addToHistory(`pop ${result}`);
-}
+let queueWidget = new QueueWidget(canvas);
+let historyContainer = new HistoryContainer(story);
+const queue: DecQueue = new DecQueue();
+let funcContainer = new FunctionsContainer(addButton,popButton,textBox,historyContainer,queue);
 
-function handleScale(scaleMult: number, zoom: boolean, origin: Point) {
-    scaleMult = zoom ? scaleMult : 1 / scaleMult;
-
-    let imatrix: DOMMatrix = currMatrix.inverse();
-    origin.x = origin.x * imatrix.a + origin.y * imatrix.c + imatrix.e;
-    origin.y = origin.x * imatrix.b + origin.y * imatrix.d + imatrix.f;
-
-    if (scale * scaleMult >= 0.125 && scale * scaleMult <= 4) {
-        currMatrix.scaleSelf(scaleMult, scaleMult, 1, origin.x, origin.y);
-        scale *= scaleMult;
-    }
-}
-
-addButton.onclick = () => {
-    handleAddItem(textBox);
-};
-
-popButton.onclick = handlePopItem;
-
-textBox.onkeydown = (e) => {
-    if (e.key === "Enter") handleAddItem(textBox);
-};
-
-canvas.onwheel = (e: WheelEvent) => {
-    handleScale(1.1, e.deltaY < 0, getPoint(canvas, { x: e.clientX, y: e.clientY }));
-};
-
-canvas.onmousemove = (e: MouseEvent) => {
-    if (rectContainPoint(RESET_POS, RESET_WIDTH, RESET_HEIGTH, getPoint(canvas, { x: e.clientX, y: e.clientY }))) {
-        canvas.style.cursor = "pointer";
-    } else {
-        canvas.style.cursor = "move";
-    }
-};
-
-let savedOnMouseMove = canvas.onmousemove;
-
-canvas.onmousedown = (e: MouseEvent) => {
-    if (rectContainPoint(RESET_POS, RESET_WIDTH, RESET_HEIGTH, getPoint(canvas, { x: e.clientX, y: e.clientY }))) {
-        scale = 1;
-        currMatrix = new DOMMatrix();
-    }
-
-    canvas.onmousemove = (e: MouseEvent) => {
-        let offsetX: number = e.movementX;
-        let offsetY: number = e.movementY;
-        currMatrix.translateSelf(offsetX / scale, offsetY / scale);
-    };
-
-    canvas.onmouseup = () => {
-        canvas.onmousemove = savedOnMouseMove;
-        canvas.onmouseup = null;
-    };
-};
-
-canvas.onmouseup = () => {
-    canvas.onmousemove = savedOnMouseMove;
-    canvas.onmouseup = null;
-};
-
-function loop(time: DOMHighResTimeStamp): void {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawResetButton(ctx);
-    for (let i = 0; i < trace.length; ++i) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.save();
-        ctx.setTransform(currMatrix);
-        drawItems(ctx, trace[i]);
-        ctx.restore();
-        drawResetButton(ctx);
-    }
+function loop(time: DOMHighResTimeStamp) {
+    queueWidget.render(queue);
     window.requestAnimationFrame(loop);
 }
 
-window.addEventListener(
-    "load",
-    () => {
-        updateSizes(canvas);
-        initContext(ctx);
-        window.requestAnimationFrame(loop);
-    },
-    false
-);
-
-window.addEventListener(
-    "resize",
-    () => {
-        updateSizes(canvas);
-        initContext(ctx);
-        window.requestAnimationFrame(loop);
-    },
-    false
-);
+window.requestAnimationFrame(loop);
